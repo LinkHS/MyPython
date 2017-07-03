681886a971830a82d22d2302718966b366657e98
austin.dawei <dawei.yang@hobot.cc>
2017-07-03 09:51:43 +0800

    fix mismatched points and update

--- DMS/get_landmarks_from_anno.py
+++ DMS/get_landmarks_from_anno.py
@@ -1,18 +1,32 @@
+### python get_landmarks_from_anno.py anno.json out.txt
 import json
 import math
+import argparse
+import os
 
 
-fn_json = '/Volumes/TF128-OV/uhome/DMS_Video/20170418_JOC/10AM/1797900845.json'
-fn_ldmk = 'ldmk.txt'
 ### minimum number of valid landmark numbers, that is invisible number should less than (total - min_lmk_num)
 # min_lmk_num = 68
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('fn_json', type=str, help='json file to read')
+    parser.add_argument('fn_ldmk', type=str, help='landmark result to save')
+    parser.add_argument('-img_path_prefix', type=str, default=None, help='[option]add path before image name in the fn_ldmk')
+    parser.add_argument('-debug_print', type=bool, default=False, help='[option]for debug')
+    parser.add_argument('-debug_imshow', type=bool, default=False, help='[option]for debug')
+    args = parser.parse_args()
+
+    if args.img_path_prefix != None and args.img_path_prefix[-1] != os.sep:
+        args.img_path_prefix += os.sep
+    return args
 
 
 ### points_72_list format: [(x1,y1), (x2,y2), ...]
 ### attrs_72_list format: ['', 'invisible', ...]
 def convertLdmkFrom72To21(points_72_list, attrs_72_list):
     points_21_paired_72 = [0,  2,  3,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-    points_72_paired_21 = [22, 26, 39, 43, 16, 21, 17, 30, 38, 34, 50, 57, 53, 58, 60, 67, 70, 64, 62]
+    points_72_paired_21 = [22, 26, 39, 43, 13, 21, 17, 30, 38, 34, 50, 57, 53, 58, 60, 67, 70, 64, 62]
     points_21_list = [(-1.0, -1.0) for n in range(21)]
 
     for i in range(len(points_72_paired_21)):
@@ -23,18 +37,18 @@ def convertLdmkFrom72To21(points_72_list, attrs_72_list):
         else:
             points_21_list[idx_21] = points_72_list[idx_72]
 
-    if attrs_72_list[24] != '' or attrs_72_list[23] != '':
+    if attrs_72_list[24] != '' or attrs_72_list[28] != '':
         points_21_list[1] = (-1, -1)
     else:
-        xsum = points_72_list[24][0] + points_72_list[23][0]
-        ysum = points_72_list[24][1] + points_72_list[23][1]
+        xsum = points_72_list[24][0] + points_72_list[28][0]
+        ysum = points_72_list[24][1] + points_72_list[28][1]
         points_21_list[1] = (xsum/2, ysum/2)
 
-    if attrs_72_list[32] != '' or attrs_72_list[36] != '':
+    if attrs_72_list[41] != '' or attrs_72_list[46] != '':
         points_21_list[4] = (-1, -1)
     else:
-        xsum = points_72_list[32][0] + points_72_list[36][0]
-        ysum = points_72_list[32][1] + points_72_list[36][1]
+        xsum = points_72_list[41][0] + points_72_list[45][0]
+        ysum = points_72_list[41][1] + points_72_list[45][1]
         points_21_list[4] = (xsum/2, ysum/2)
 
     return points_21_list
@@ -111,8 +125,11 @@ class Ldmk72Parser:
 
 
 if __name__ == '__main__':
-    ldmk_parser = Ldmk72Parser(fn_json)
-    out_f = open(fn_ldmk, 'w')
+    args = parse_args()
+
+    print "open", args.fn_json
+    ldmk_parser = Ldmk72Parser(args.fn_json)
+    out_f = open(args.fn_ldmk, 'w')
     for i in range(0, ldmk_parser.__len__()):
         image_name, points_list, attrs_list = ldmk_parser.getAnnoRes(i)
         if image_name != None:
@@ -123,6 +140,8 @@ if __name__ == '__main__':
                 out_line += ' '
                 out_line += ' '.join(['%.2f' % b for b in ldmk21[j]])
 
+            if args.img_path_prefix is not None:
+                image_name = args.img_path_prefix + image_name 
             out_f.write(image_name + out_line + '\n')
     out_f.truncate(out_f.tell()-1)
     out_f.close()
